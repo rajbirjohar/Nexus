11df09aa24642dbaded0da326cde448031916449
rajbirjohar <github@rajbir.io>
2021-10-07 20:46:21 -0700

    :nail_care: Added global styles and setup dark mode structure

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import styles from '@/styles/footer.module.css'
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Made by five students.</p>
+    </footer>
+  )
+}
