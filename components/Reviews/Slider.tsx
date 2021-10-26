ad1ca096fd51dec5a50d0a5121767b435a7e2324
rajbirjohar <github@rajbir.io>
2021-10-25 19:28:15 -0700

    :white_check_mark: Fixed slider and added empty field check

--- components/Reviews/Slider.tsx
+++ components/Reviews/Slider.tsx
@@ -13,28 +13,30 @@ const useSlider = (min, max, defaultState, label, id) => {
     console.log('useSlider:', slide)
   }, [setSlide, slide])
 
-  const Slider = () => (
-    <>
-      <label htmlFor="_difficulty">
-        <strong>!! IMPT: Slider sets the previous value</strong>
-        <br />
-        <strong>
-          {label} {slide}
-        </strong>
-      </label>
-      <input
-        type="range"
-        name="_difficulty"
-        id={id}
-        min={min}
-        max={max}
-        step={1}
-        defaultValue={slide} // but instead pass state value as default value
-        onChange={(e) => console.log(e.target.value)} // don't set state on all change as react will re-render
-        onMouseUp={handleSliderChange} // only set state when handle is released
-      />
-    </>
-  )
+  const Slider = (props) => {
+    return (
+      <>
+        <label htmlFor="_difficulty">
+          <br />
+          <strong>
+            {label} {slide}
+          </strong>
+        </label>
+        <input
+          type="range"
+          name="_difficulty"
+          id={id}
+          min={min}
+          max={max}
+          step={1}
+          defaultValue={slide} // but instead pass state value as default value
+          onChange={(e) => console.log(e.target.value)} // don't set state on all change as react will re-render
+          // onMouseUp={handleSliderChange} // only set state when handle is released
+          onMouseUp={props['onHandleChange']}
+        />
+      </>
+    )
+  }
   return [slide, Slider, setSlide]
 }
 
