58ce2cdde442cb605c36eac632077f9506331ee0
Robert Rivera <robert.psych288@gmail.com>
2021-11-24 12:37:26 -0800

    :eyes: Added search bar to other pages

--- components/Events/ListAllEvents.tsx
+++ components/Events/ListAllEvents.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import Image from 'next/image'
 import useSWR from 'swr'
 import Loader from '../Skeleton'
@@ -10,6 +11,7 @@ export default function ListAllEvents() {
   const { data, error } = useSWR('/api/events/eventfetch', Fetcher, {
     refreshInterval: 1000,
   })
+  const [searchValue, setSearchValue] = useState('')
   if (error) {
     return (
       <div className={formstyles.serverdown}>
@@ -29,11 +31,20 @@ export default function ListAllEvents() {
   if (!data) {
     return <Loader />
   }
+  const filteredEvents = Object(data.events).filter(
+    (event) =>
+      event.eventName.toLowerCase().includes(searchValue.toLowerCase()) ||
+      event.organizationName
+        .toLowerCase()
+        .includes(searchValue.toLowerCase()) ||
+      event.eventDetails.toLowerCase().includes(searchValue.toLowerCase())
+  )
   return (
-    <div className={cardstyles.grid}>
-      {data.events.length === 0 && (
+    <div>
+      {data.events.length === 0 ? (
         <div className={formstyles.noreviews}>
-          <p>No new events! Check back later.</p>
+          <p>Create an event!</p>
+
           <Image
             src={'/assets/post2.svg'}
             height={300}
@@ -41,18 +52,54 @@ export default function ListAllEvents() {
             alt="Post Image"
           />
         </div>
+      ) : (
+        <div className={formstyles.searchWrapper}>
+          <input
+            aria-label="Enabled Searchbar"
+            type="text"
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Search by name, club, or details."
+            className={formstyles.search}
+          />
+          <svg className={formstyles.searchIcon}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </svg>
+        </div>
       )}
-      {data.events.map((newEvent) => (
-        <EventCard
-          key={newEvent._id}
-          organizationName={newEvent.organizationName}
-          eventName={newEvent.eventName}
-          eventDetails={newEvent.eventDetails}
-          eventId={newEvent._id}
-          startDate={newEvent.eventStartDate}
-          endDate={newEvent.eventEndDate}
-        />
-      ))}
+      <div className={cardstyles.grid}>
+        {!filteredEvents.length && data.events.length !== 0 && (
+          <>
+            <p>
+              No events found!
+              <br />
+              <cite>— Robert</cite>
+            </p>
+          </>
+        )}
+        {filteredEvents.map((newEvent) => (
+          <EventCard
+            key={newEvent._id}
+            organizationName={newEvent.organizationName}
+            eventName={newEvent.eventName}
+            eventDetails={newEvent.eventDetails}
+            eventId={newEvent._id}
+            startDate={newEvent.eventStartDate}
+            endDate={newEvent.eventEndDate}
+          />
+        ))}
+      </div>
     </div>
   )
 }
