bffafa75c0a6116633d8484da29b0146f176e92a
rajbirjohar <rajbirjohar1@gmail.com>
2021-12-29 12:20:49 -0800

    :tipping_hand_person: Converted set role form to Formik

---
+++ components/Profile/SetRoleForm.tsx
@@ -0,0 +1,98 @@
+import Router from 'next/router'
+import { Formik, Form, Field, ErrorMessage, FormikErrors } from 'formik'
+import toast from 'react-hot-toast'
+import styles from '@/styles/form.module.css'
+
+interface Role {
+  userId: string
+  _role: string
+}
+
+export default function SetRoleForm({ userId }) {
+  const initialValues: Role = {
+    userId: userId,
+    _role: '',
+  }
+  console.log(initialValues)
+  const sendData = async (userRoleData) => {
+    const response = await fetch('/api/users/setrole', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userRoleData: userRoleData }),
+    })
+    const data = await response.json()
+
+    if (response.status === 200) {
+      toast.success("You've set your role!")
+      Router.reload()
+    } else {
+      toast.error(
+        'Uh oh. Something happened. Please contact us if this persists.'
+      )
+    }
+  }
+  return (
+    <div>
+      <h3>Student or Professor?</h3>
+      <p>
+        <strong>
+          Please tell us what your position is at UCR.
+          <br />
+          You won&#39;t be able to change this after you submit.
+        </strong>
+        <br />
+        Please enter <strong>&#34;student&#34;</strong> if you are a student.
+        <br />
+        Please enter <strong>&#34;professor&#34;</strong> if you are a
+        professor.
+      </p>
+      <Formik
+        validateOnBlur={false}
+        initialValues={initialValues}
+        validate={(values: Role) => {
+          let errors: FormikErrors<Role> = {}
+          if (!values._role) {
+            errors._role = 'Required'
+          } else if (
+            values._role !== 'student' &&
+            values._role !== 'professor'
+          ) {
+            errors._role = 'Incorrect answer'
+          }
+          return errors
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          sendData(values)
+          setTimeout(() => {
+            setSubmitting(false)
+          }, 400)
+        }}
+      >
+        {({ values, isSubmitting }) => (
+          <Form>
+            <div className={styles.inputheader}>
+              <label htmlFor="_comment">
+                <strong>Role:</strong>
+              </label>
+              <ErrorMessage name="_role">
+                {(message) => <span className={styles.error}>{message}</span>}
+              </ErrorMessage>
+            </div>
+            <Field type="text" name="_role" placeholder="Role" maxLength={10} />
+            <span className={styles.actions}>
+              <button
+                className={styles.primary}
+                type="submit"
+                disabled={isSubmitting}
+              >
+                Set Role!
+              </button>
+            </span>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  )
+}
