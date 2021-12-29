a1f401fc25c218d67e616e51839d61f6126aad68
rajbirjohar <rajbirjohar1@gmail.com>
2021-12-28 17:27:45 -0800

    :nail_care: Swapped icon order

--- components/Profile/NotificationCard.tsx
+++ components/Profile/NotificationCard.tsx
@@ -51,13 +51,15 @@ export const NotificationCard = ({ notifId, type, message, createdAt }) => {
       <button onClick={deleteNotif} className={formstyles.deleteicon}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
         >
           <path
-            fillRule="evenodd"
-            d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
-            clipRule="evenodd"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
           />
         </svg>
       </button>
