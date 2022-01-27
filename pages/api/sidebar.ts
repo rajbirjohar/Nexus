7dd89481a962003a2b64c4c4386d6b506105c592
rajbirjohar <rajbirjohar1@gmail.com>
2022-01-27 05:37:17 -0800

    :rocket: Populated discover tab

--- pages/api/sidebar.ts
+++ pages/api/sidebar.ts
@@ -1,19 +1,28 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import clientPromise from '@/lib/mongodb'
 
-export default async function fetchAllEvents(
+export default async function fetchDiscoverSidebar(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const isConnected = await clientPromise
   const db = isConnected.db(process.env.MONGODB_DB)
 
-  const events = await db
+  const discover = await db
     .collection('events')
-    .find({
-      eventEndDate: { $gte: new Date() },
-    })
+    .aggregate([
+      {
+        $match: {
+          eventEndDate: { $gte: new Date() },
+        },
+      },
+      {
+        $sample: {
+          size: 3,
+        },
+      },
+    ])
     .sort({ eventStartDate: 1 })
     .toArray()
-  return res.status(200).json({ events })
+  return res.status(200).json({ discover })
 }
