de1df59b0045f1cf42631c2c313498077bca2601
rajbirjohar <rajbirjohar1@gmail.com>
2022-02-07 16:21:38 -0800

    :x: Admins can now remove members

--- components/Organizations/RemoveMemberForm.tsx
+++ components/Organizations/RemoveMemberForm.tsx
@@ -2,7 +2,7 @@ import Router from 'next/router'
 import toast from 'react-hot-toast'
 import formstyles from '@/styles/form.module.css'
 
-export default function RemoveMemberForm({
+export function RemoveMemberForm({
   memberId,
   organizationId,
   organizationName,
@@ -36,5 +36,42 @@ export default function RemoveMemberForm({
         <button type="submit">Leave {organizationName}</button>
       </span>
     </form>
+  )
+}
+
+// I'm too lazy to create conditional renders
+// Literally the same code as above except for how the delete buttons are displayed
+export function RemoveMemberAdminForm({
+  memberId,
+  organizationId,
+  organizationName,
+}) {
+  const member = { memberId, organizationName, organizationId }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    sendData(member)
+  }
+  const sendData = async (memberData) => {
+    const response = await fetch('/api/organizations/memberremove', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ memberData: memberData }),
+    })
+    await response.json
+    if (response.status === 200) {
+      toast.success(`You removed this member.`)
+      Router.reload()
+    } else {
+      toast.error(
+        'Uh oh, something went wrong. If this persists, please let us know.'
+      )
+    }
+  }
+  return (
+    <span onClick={handleSubmit} className={formstyles.deletecomment}>
+      Remove
+    </span>
   )
 }
